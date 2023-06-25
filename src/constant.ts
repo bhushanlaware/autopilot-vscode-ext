8c602feae971cf5dbe9f6c39da1f4fcd1872e27e
Bhushan Laware <bhushanlaware@gmail.com>
2023-06-25 09:23:20 +0530

    10 lines are good

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,6 @@
 import { ChatConfig, CompletionConfig } from "./types";
 
-export const MAX_PREVIOUS_LINE_FOR_PROMPT = 20;
+export const MAX_PREVIOUS_LINE_FOR_PROMPT = 10;
 export const MAX_ALLOWED_CACHED_SUGGESTION_DIFF = 3;
 export const MAX_ALLOWED_LINE = 50;
 export const SELECTED_CODE_MAX_LENGTH = 1000;
