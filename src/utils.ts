4f7d4438669d146d93d65abc73e111f21325da16
Bhushan Laware <bhushanlaware@gmail.com>
2023-10-12 10:35:34 +0530

    better prompting

--- src/utils.ts
+++ src/utils.ts
@@ -103,7 +103,7 @@ export function getOpenedFiles() {
 
 export function getInstruction(files: Files): string {
   const instructions = [];
-  instructions.push("INSTRUCTION:You are a helpful assistant. Keep the answers shorts");
+  instructions.push("INSTRUCTION:You are a helpful developer assistant");
 
   // fileContext
   instructions.push("Please refer the relative files in context to user query.");
